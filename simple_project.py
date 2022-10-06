3a743ab1a0e54b25519f7ca7adb58339cfb490c2
RAUT0911 <112767988+RAUT0911@users.noreply.github.com>
2022-10-06 16:04:25 +0530

    Update and rename README.md to simple_project.py

---
+++ simple_project.py
@@ -0,0 +1,39 @@
+# python-mobile-app
+""" Creating basic layout using Gredlayout , label , coloumn , app, widget and Library in KIVY with the help of pycharm editor devlope a simple input-output ,details,button."""
+import livy 
+from kiy.app import App
+from kivy.uix.gridlyout import Gridlayout
+from kivy.uix.label import Label
+from kivy.uix.textinput import Textinput 
+from kivy.uix.button import Button
+
+class PrashantGrid(Gridlayout):
+   def __init__(self,**kwargs):
+   super(Prasahnt,self).__init__()
+   self.cols=2
+  
+   self.add_widget(Label(text="Students Name: "))
+   self.s_name=Textinput()
+   self.add_widget(self.s_name)
+   self.add_widget(Label(text="Students Marks: "))
+   self.s_marks=Textinput()
+   self.add_widget(self.s_marks)
+   self.add_widget(Label(text="Student Gender: "))
+   self.s_gender=Textinput()
+   self.add_widget(self.s_gender)
+   self.press=Button(text="Click Me ")
+   self.press.bind(on_press=self.click_me)
+   self.add_widget(self.press)
+   
+   def click_me(self,instance):
+   print("You have enter Details Succesfully !! ")
+   print("Name of student is  : ",self.s_name.text)
+   print("marks of student are  : ",self.s_marks.text)
+   print("Gender of student is  : ",self.s_gender.text)
+   
+class PrashantApp(App):
+def build(self):
+return PrashantGrid()
+
+if __name__== "__main__":
+  PrashantApp().run()
