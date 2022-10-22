565517ba1f21d22444691f9de331431fff508ad5
RAUT0911 <112767988+RAUT0911@users.noreply.github.com>
2022-10-22 13:57:33 +0530

    Update simple_project.py

--- simple_project.py
+++ simple_project.py
@@ -9,31 +9,31 @@ from kivy.uix.button import Button
 
 class PrashantGrid(Gridlayout):
    def __init__(self,**kwargs):
-   super(Prashant,self).__init__()
-   self.cols=2
+     super(Prashant,self).__init__()
+     self.cols=2
+     self.add_widget(Label(text="Students Name: "))
+     self.s_name=Textinput()
+     self.add_widget(self.s_name)
+     self.add_widget(Label(text="Students Marks: "))
+     self.s_marks=Textinput()
+     self.add_widget(self.s_marks)
+     self.add_widget(Label(text="Student Gender: "))
+     self.s_gender=Textinput()
+     self.add_widget(self.s_gender)
+     self.press=Button(text="Click Me ")
+     self.press.bind(on_press=self.click_me)
+     self.add_widget(self.press)
   
-   self.add_widget(Label(text="Students Name: "))
-   self.s_name=Textinput()
-   self.add_widget(self.s_name)
-   self.add_widget(Label(text="Students Marks: "))
-   self.s_marks=Textinput()
-   self.add_widget(self.s_marks)
-   self.add_widget(Label(text="Student Gender: "))
-   self.s_gender=Textinput()
-   self.add_widget(self.s_gender)
-   self.press=Button(text="Click Me ")
-   self.press.bind(on_press=self.click_me)
-   self.add_widget(self.press)
    
    def click_me(self,instance):
-   print("You have enter Details Succesfully !! ")
-   print("Name of student is  : ",self.s_name.text)
-   print("marks of student are  : ",self.s_marks.text)
-   print("Gender of student is  : ",self.s_gender.text)
+        print("You have enter Details Succesfully !! ")
+        print("Name of student is  : ",self.s_name.text)
+        print("marks of student are  : ",self.s_marks.text)
+        print("Gender of student is  : ",self.s_gender.text)
    
 class PrashantApp(App):
-def build(self):
-return PrashantGrid()
+   def build(self):
+     return PrashantGrid()
 
 if __name__== "__main__":
   PrashantApp().run()
